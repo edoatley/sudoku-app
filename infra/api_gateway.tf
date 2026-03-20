93b1d08378f9d68d56e02cd9988258911a0d4c94
edoatley <edoatley@gmail.com>
2026-03-20 15:55:15 +0000

    smoke fixes

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -26,7 +26,7 @@ resource "aws_apigatewayv2_integration" "lambda" {
   api_id                 = aws_apigatewayv2_api.sudoku.id
   integration_type       = "AWS_PROXY"
   integration_uri        = aws_lambda_alias.live.arn
-  payload_format_version = "2.0"
+  payload_format_version = "1.0"
 }
 
 resource "aws_apigatewayv2_route" "default" {
