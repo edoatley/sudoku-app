4770cca84ce6cfd7a9d7f71fbb41194bfec497b4
edoatley <edoatley@gmail.com>
2026-04-01 08:04:57 +0100

    fix dev tools flag

--- infra/amplify.tf
+++ infra/amplify.tf
@@ -29,6 +29,7 @@ resource "aws_amplify_app" "sudoku" {
     VITE_COGNITO_CLIENT_ID    = local.cognito_web_client_id
     VITE_COGNITO_DOMAIN       = "${local.cognito_domain}.auth.eu-west-2.amazoncognito.com"
     VITE_ENABLE_IMPORT        = local.is_default ? "false" : "true"
+    VITE_DEV_TOOLS            = local.is_default ? "false" : "true"
   }
 
   auto_branch_creation_config {
